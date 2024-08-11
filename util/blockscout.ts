fc4347480d8ca0c605c56b11fcacb840af9ddfbe
Chris Buonocore <chrisdistrict@gmail.com>
2024-08-10 23:46:19 -0400

    fix url

--- util/blockscout.ts
+++ util/blockscout.ts
@@ -4,5 +4,5 @@ export const BLOCKSCOUT_MAP: Record<string, string> = {
 	// chainId => blockscout domain
 	[baseSepolia.id]: "https://base-sepolia.blockscout.com",
 	[base.id]: "https://base.blockscout.com",
-	[modeTestnet.id]: "https//sepolia.explorer.mode.network",
+	[modeTestnet.id]: "https://sepolia.explorer.mode.network",
 }
